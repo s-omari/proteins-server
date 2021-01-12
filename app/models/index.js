f823805940d391e810146dfb22196b8698c12af6
saleh omari <s.omari.jor@gmail.com>
2021-01-12 10:30:19 -0800

    added role and user modules

--- app/models/index.js
+++ app/models/index.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
-
 mongoose.Promise = global.Promise;
 
+
+const dbConfig = require('../config/db.config')
 const db = {
     mongoose: mongoose,
-    url : "mongodb://localhost:27017/bioreader_db",
-    proteins: require("./protein.model.js")
+    url : `mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`,
+    proteins: require("./protein.model.js"),
+    roles: require("./role.model.js"),
+    users: require("./user.model.js"),
 };
+
+db.ROLES = ["user", "admin", "moderator"];
+
 
 module.exports = db;
--- server.js
+++ server.js
@@ -21,7 +21,8 @@ db.mongoose.connect(
         useUnifiedTopology: true
     }
 ).then(() => {
-    console.log('connected to db')
+    console.log('connected to db');
+    initial();
 }).catch(err => {
     console.log('could not connect to db', err);
     process.exit();
@@ -34,3 +35,43 @@ const PORT = process.env.port || 8080;
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
 })
+
+
+
+
+const Role = db.roles;
+function initial() {
+    Role.estimatedDocumentCount((err, count) => {
+      if (!err && count === 0) {
+        new Role({
+          name: "user"
+        }).save(err => {
+          if (err) {
+            console.log("error", err);
+          }
+  
+          console.log("added 'user' to roles collection");
+        });
+  
+        new Role({
+          name: "moderator"
+        }).save(err => {
+          if (err) {
+            console.log("error", err);
+          }
+  
+          console.log("added 'moderator' to roles collection");
+        });
+  
+        new Role({
+          name: "admin"
+        }).save(err => {
+          if (err) {
+            console.log("error", err);
+          }
+  
+          console.log("added 'admin' to roles collection");
+        });
+      }
+    });
+  }
